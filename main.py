4aeb363fb65fbe3dac32a0393ec885535f6aa525
said ech-chadi <saidech@gmail.com>
2023-05-16 15:42:48 +0100

    modification de revision

---
+++ main.py
@@ -0,0 +1,15 @@
+import requests
+from pprint import pprint
+
+API_Key = 'cb771e45ac79a4e8e2205c0ce66ff633'
+#API_Key = '30e91ab40822529e7f8c321cb0ae3697'
+
+city = input("Enter a city ")
+
+base_url = "http://api.openweathermap.org/data/2.5/weather?appid=" + API_Key + "&q=" + city
+
+weather_data = requests.get(base_url).json()
+
+# -rev 2.0
+
+pprint(weather_data)
